0b312a076613fd1f37e5e27d58bb9ed75357c951
Andrei Malinkovich <rewwerse@gmail.com>
2014-05-05 01:43:26 +0200

    index changes

--- js.js
+++ js.js
@@ -105,7 +105,7 @@ AJAX('GET', 'http://54.72.3.96:3000/techtalks').then(function (data) {
             var lectorCard = JSON.parse(response);
             tableRows += "<tr><td><strong>" + item.title + "</strong></td><td>"
                 + item.lector + "</td>"
-                + "</td><td><a href='mailto:'" + lectorCard.email + "'>" + lectorCard.email + "</a></td></tr>";
+                + "</td><td><a href='mailto:'"+lectorCard.email + "'>" + lectorCard.email + "</a></td></tr>";
             table.innerHTML = tableRows;
         })
 
