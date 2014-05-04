a9427b5f1e2e5b06860c41584e315a668afed8da
Andrei Malinkovich <rewwerse@gmail.com>
2014-05-04 22:39:13 +0200

    index changes

--- js.js
+++ js.js
@@ -97,7 +97,8 @@ AJAX('GET', 'http://54.72.3.96:3000/techtalks').then(function(data){
         if (techTalks[i].title != techTalks[i+1].title) {
             resultTechTalks.push(techTalks[i].title);
         }
-    };
+    }
+    debugger;
     console.log('123');
     console.log(resultTechTalks);
 /*
