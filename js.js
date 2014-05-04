aa7debc9cee3fa4d8b4521f975d17bb50a7f3577
Andrei Malinkovich <rewwerse@gmail.com>
2014-05-04 17:06:16 +0200

    index changes

--- js.js
+++ js.js
@@ -1,4 +1,27 @@
-var xhr = new XMLHttpRequest();
+function AJAX(type, url, contentType, sendData) {
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
 
-xhr.open('GET','http://54.72.3.96:3000/techtalks');
-xhr.send();
+        xhr.open(type, url);
+
+        xhr.onload = function () {
+            if (xhr.status == 200) {
+                resolve(xhr.response);
+            } else {
+                reject(Error(request.statusText));
+            }
+        }
+
+        xhr.onerror = function() {
+            reject(Error('Error on server!'));
+        }
+
+        if (contentType) {
+            xhr.setRequestHeader('Content-Type', contentType);
+        }
+
+        xhr.send(sendData);
+    })
+}
+
+AJAX('GET', 'http://54.72.3.96:3000/techtalks');
