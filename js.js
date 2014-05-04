f10fac0313dc972db3881e76efec186dbb09615a
Andrei Malinkovich <rewwerse@gmail.com>
2014-05-05 00:22:17 +0200

    index changes

--- js.js
+++ js.js
@@ -12,7 +12,7 @@ function AJAX(type, url, contentType, sendData) {
             }
         }
 
-        xhr.onerror = function() {
+        xhr.onerror = function () {
             reject(Error('Error on server!'));
         }
 
@@ -48,77 +48,78 @@ var record = {
 var jsonRecord = JSON.stringify(record);
 
 /*AJAX ('POST', 'http://54.72.3.96:3000/techtalks', 'application/json', jsonRecord)
-    .then(function(response){
-        var lastRecord = JSON.parse(response);
-        console.log('ADD NEW RECORD...');
-        console.log('ID: ' + lastRecord['_id']);
-        return lastRecord['_id'];
-    })
-    .then(function(techTalk){
-        AJAX('GET', 'http://54.72.3.96:3000/techtalks/' + techTalk).then(function(response){
-            console.log('READ: ', response);
-            console.log('ID: ' + techTalk)
-        })
-        return techTalk;
-    })
-    .then(function(techTalk){
-        var updatedString = JSON.stringify({'lector':'vasily_pupkin'});
+ .then(function(response){
+ var lastRecord = JSON.parse(response);
+ console.log('ADD NEW RECORD...');
+ console.log('ID: ' + lastRecord['_id']);
+ return lastRecord['_id'];
+ })
+ .then(function(techTalk){
+ AJAX('GET', 'http://54.72.3.96:3000/techtalks/' + techTalk).then(function(response){
+ console.log('READ: ', response);
+ console.log('ID: ' + techTalk)
+ })
+ return techTalk;
+ })
+ .then(function(techTalk){
+ var updatedString = JSON.stringify({'lector':'vasily_pupkin'});
 
-        AJAX('PUT', 'http://54.72.3.96:3000/techtalks/' + techTalk, '', updatedString).then(function(response){
-            console.log('UPDATE STRING LECTOR: ', updatedString);
-            console.log('ID: ' + techTalk)
-        })
-        return techTalk;
-    })
-    .then(function(techtalk){
-        AJAX('DELETE', 'http://54.72.3.96:3000/techtalks/' + techTalk);
-        console.log ('DELETE: ', techtalk);
-        console.log('ID: ' + techTalk);
-        return techTalk;
-    })*/
+ AJAX('PUT', 'http://54.72.3.96:3000/techtalks/' + techTalk, '', updatedString).then(function(response){
+ console.log('UPDATE STRING LECTOR: ', updatedString);
+ console.log('ID: ' + techTalk)
+ })
+ return techTalk;
+ })
+ .then(function(techtalk){
+ AJAX('DELETE', 'http://54.72.3.96:3000/techtalks/' + techTalk);
+ console.log ('DELETE: ', techtalk);
+ console.log('ID: ' + techTalk);
+ return techTalk;
+ })*/
 
-AJAX('GET', 'http://54.72.3.96:3000/techtalks').then(function(data){
+AJAX('GET', 'http://54.72.3.96:3000/techtalks').then(function (data) {
     var techTalks = JSON.parse(data);
     var techTalksTitles = [];
-    var resultTechTalks = [];
+    var resultTechTalks2 = [];
     var table = document.getElementById('table');
     var tableRows = '';
 
     function sortJSON(array, key) {
-        return array.sort(function(a, b) {
+        return array.sort(function (a, b) {
             var x = a[key];
             var y = b[key];
             return ((x < y) ? -1 : ((x > y) ? 1 : 0));
         });
     }
-    techTalks = sortJSON(techTalks,'title');
+
+    techTalks = sortJSON(techTalks, 'title');
 
     for (var i = 0; i < techTalks.length; i++) {
-           /* if (techTalks[i].title != techTalks[i+1].title) {
-                resultTechTalks.push(techTalks[i]);
-        }*/
-    };
-    console.log('123');
-    console.log(resultTechTalks);
-/*
-    for (var i in techTalks) {
-        techTalksTitles[i] = techTalks[i].title;
+        if (techTalks[i].title != techTalks[i + 1].title) {
+            resultTechTalks2.push(techTalks[i]);
+        }
     }
+    console.log('123');
+    console.log(resultTechTalks2);
+    /*
+     for (var i in techTalks) {
+     techTalksTitles[i] = techTalks[i].title;
+     }
 
-    techTalksTitles.sort();
+     techTalksTitles.sort();
 
-    for (var i = 0; i <= techTalksTitles.length; i++) {
-        if (techTalksTitles[i] != techTalksTitles[i + 1]){
-            resultTechTalks.push(techTalksTitles[i]);
-        };
-    };
+     for (var i = 0; i <= techTalksTitles.length; i++) {
+     if (techTalksTitles[i] != techTalksTitles[i + 1]){
+     resultTechTalks.push(techTalksTitles[i]);
+     };
+     };
 
-    console.log(resultTechTalks);
+     console.log(resultTechTalks);
 
 
-    resultTechTalks.forEach(function(item){
-        tableRows +='<tr><td><strong>'+ item +'</strong></td></tr>'
-    })
-    table.innerHTML = tableRows;
- */
+     resultTechTalks.forEach(function(item){
+     tableRows +='<tr><td><strong>'+ item +'</strong></td></tr>'
+     })
+     table.innerHTML = tableRows;
+     */
 });
