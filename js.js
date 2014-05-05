f5361f137d3ce2b89d4639f435c15052a7110762
Andrei Malinkovich <rewwerse@gmail.com>
2014-05-05 02:00:58 +0200

    index changes

--- js.js
+++ js.js
@@ -83,7 +83,6 @@ var jsonRecord = JSON.stringify(record);
 
 AJAX('GET', 'http://54.72.3.96:3000/techtalks').then(function (data) {
     var techTalks = JSON.parse(data);
-    var techTalksTitles = [];
     var resultTechTalks = [];
     var table = document.getElementById('table');
     var tableRows = '';
